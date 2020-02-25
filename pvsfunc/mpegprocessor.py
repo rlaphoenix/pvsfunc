90c18112a076efdb1f098be19605813bb4fdf827
PRAGMA <17136956+imPRAGMA@users.noreply.github.com>
2020-02-25 22:37:17 +0000

    Revert "v2.0.5-post1 - :poop: whoops"
    This reverts commit d63ae45f7ab80bd4c38b476f93a9f4d3c648c755.

--- pvsfunc/mpegprocessor.py
+++ pvsfunc/mpegprocessor.py
@@ -58,7 +58,7 @@ class MpegProcessor():
                 d2v_path = f"{os.path.splitext(d2v_path)[0]}.d2v"
                 if not os.path.exists(d2v_path):
                     # couldn't find d2v, generate one on-the-fly
-                    mpg_path = f"{os.path.join(os.path.dirname(d2v_path), unidecode.unidecode(os.path.splitext(os.path.basename(d2v_path))[0]))}.mpg"
+                    mpg_path = unidecode.unidecode(f"{os.path.splitext(d2v_path)[0]}.mpg")
                     if not os.path.exists(mpg_path):
                         # couldn't find mpg, generate one on-the-fly
                         subprocess.run([
