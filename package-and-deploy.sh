f0e6ebb1477b808264600668d6411a4db16a29ed
PHOENiX <rlaphoenix@pm.me>
2020-10-07 19:35:01 +0100

    Update package-and-deploy.sh

--- package-and-deploy.sh
+++ package-and-deploy.sh
@@ -1,5 +1,5 @@
 rm -r build
 rm -r dist
 rm -r "pvsfunc.egg-info"
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
+python setup.py sdist bdist_wheel
+python -m twine upload dist/*
