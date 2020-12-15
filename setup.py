bf7aeac066521285be9b5cf20b231fa5ca0b7c68
PHOENiX <rlaphoenix@pm.me>
2020-12-15 22:37:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pvsfunc",
-    version="3.8.0",
+    version="3.8.1",
     author="PHOENiX",
     author_email="pragma.exe@gmail.com",
     description="PHOENiX's compilation of VapourSynth Script's and Functions",
