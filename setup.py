a97cfe7a63bee2d17f5cd23e818073aa8a396968
PHOENiX <rlaphoenix@pm.me>
2020-12-27 22:20:29 +0000

    Bump to v3.13.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "rt") as f:
 
 setup(
     name="pvsfunc",
-    version="3.12.1",
+    version="3.13.0",
     author="PHOENiX",
     author_email="pragma.exe@gmail.com",
     description="PHOENiX's compilation of VapourSynth Script's and Functions",
