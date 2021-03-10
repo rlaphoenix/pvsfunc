3f761684f6e41a8f6fa90b3bad3a3e0beadff77c
PHOENiX <rlaphoenix@pm.me>
2021-03-10 19:01:09 +0000

    helpers: Create `list_select_every()`

--- pvsfunc/helpers.py
+++ pvsfunc/helpers.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import shutil
 import subprocess
@@ -159,3 +160,14 @@ def calculate_par(width: int, height: int, aspect_ratio_w: int, aspect_ratio_h: 
     par_w = int(par_w / par_gcd)
     par_h = int(par_h / par_gcd)
     return "%d:%d" % (par_w, par_h)
+
+
+def list_select_every(data: list, cycle: int, offsets: list, inverse: bool = False):
+    """
+    Same as VapourSynth's core.std.SelectEvery but for generic list data, and inverse.
+    Don't use this as a replacement to core.std.SelectEvery, this should only be used on generic list data.
+    """
+    data = [data[i:i + cycle] for i in range(0, len(data), cycle)]
+    data = [[x for n, x in enumerate(c) if (n in offsets) ^ inverse] for c in data]
+    data = list(itertools.chain.from_iterable(data))  # flatten list of lists
+    return data
