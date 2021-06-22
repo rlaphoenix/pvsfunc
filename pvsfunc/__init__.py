82b61e8d214e293cee423ef59c8211d27a455e3b
rlaphoenix <rlaphoenix@pm.me>
2021-06-23 00:47:13 +0100

    Remove deleted PSourcer from `__init__`

--- pvsfunc/__init__.py
+++ pvsfunc/__init__.py
@@ -2,8 +2,7 @@ from pvsfunc.pd2v import PD2V
 from pvsfunc.pdebox import PDebox
 from pvsfunc.pdecimate import PDecimate
 from pvsfunc.pdeinterlacer import PDeinterlacer
-from pvsfunc.psourcer import PSourcer
 from pvsfunc.pkernel import PKernel
 from pvsfunc.pls import PLS
 
-__ALL__ = (PD2V, PDebox, PDecimate, PDeinterlacer, PKernel, PSourcer, PLS)
+__ALL__ = (PD2V, PDebox, PDecimate, PDeinterlacer, PKernel, PLS)
