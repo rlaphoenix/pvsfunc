28caf44d5abe7622392e027267b87f9ca10518a5
rlaphoenix <rlaphoenix@pm.me>
2021-06-23 00:45:31 +0100

    Add missing PKernel import in `__init__`

--- pvsfunc/__init__.py
+++ pvsfunc/__init__.py
@@ -3,3 +3,4 @@ from pvsfunc.pdebox import PDebox
 from pvsfunc.pdecimate import PDecimate
 from pvsfunc.pdeinterlacer import PDeinterlacer
 from pvsfunc.psourcer import PSourcer
+from pvsfunc.pkernel import PKernel
