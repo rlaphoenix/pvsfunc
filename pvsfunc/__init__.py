3a9c2ac603848509fe04eccabec69778f4c92174
rlaphoenix <rlaphoenix@pm.me>
2021-06-23 03:02:16 +0100

    Fix __ALL__ import for PLWI

--- pvsfunc/__init__.py
+++ pvsfunc/__init__.py
@@ -2,6 +2,6 @@ from pvsfunc.pd2v import PD2V
 from pvsfunc.pdebox import PDebox
 from pvsfunc.pdecimate import PDecimate
 from pvsfunc.pkernel import PKernel
-from pvsfunc.pls import PLS
+from pvsfunc.plwi import PLWI
 
-__ALL__ = (PD2V, PDebox, PDecimate, PKernel, PLS)
+__ALL__ = (PD2V, PDebox, PDecimate, PKernel, PLWI)
