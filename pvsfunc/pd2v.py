cf6401187597ad2bb4c19b002d4bc5467c741f1a
rlaphoenix <pragma.exe@gmail.com>
2021-12-27 05:13:02 +0000

    PD2V: Only use AssumeFPS for progressive sections if VFR

--- pvsfunc/pd2v.py
+++ pvsfunc/pd2v.py
@@ -282,6 +282,10 @@ class PD2V:
                             self.clip[x[0]:x[-1] + 1],
                             fpsnum=wanted_fps_num,
                             fpsden=self.clip.fps.denominator
+                        ) if self.vfr else core.std.SelectEvery(
+                            self.clip[x[0]:x[-1] + 1],
+                            cycle,
+                            offsets
                         )
                     ) for x in progressive_sections
                 ] + [
